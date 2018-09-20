272295fa5b1d5ff96a107b5d885b3abc1172995f
Gerald Bryant <hakeem1106@gmail.com>
2018-09-20 15:27:55 -0400

    should work

--- elements.js
+++ elements.js
@@ -1,6 +1,8 @@
 var map;
 
 function portfolioMap() {
-    map = new google.maps.Map(document.getElementById('app')
-    )
+    map = new google.maps.Map(document.getElementById('app'), {
+        center: { lat: 25.458, lng: 243.600 },
+        zoom: 8
+    });
 };
