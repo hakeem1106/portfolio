bf3939e4f81a44241593a2c5170d6d39dedecdd7
Gerald Bryant <hakeem1106@gmail.com>
2018-09-28 23:15:48 -0400

    updating webpack

--- docs/src/webpack.config.js
+++ docs/src/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   entry: './scripting.js',
+   entry: './scripting.jsx',
    output: {
       path: path.join(__dirname, '/bundle'),
       filename: 'index_bundle.js'
