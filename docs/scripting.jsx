ae353097485b66346a83c8735c1574af6e5c3499
Gerald Bryant <hakeem1106@gmail.com>
2018-09-29 14:09:53 -0400

    changing react from cdn

--- docs/scripting.jsx
+++ docs/scripting.jsx
@@ -1,5 +1,5 @@
-import React from 'react.production.min.js';
-import ReactDOM from 'react-dom.production.min.js';
-import App from './app.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
