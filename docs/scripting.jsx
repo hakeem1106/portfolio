3d596bfa1ca4d9b7f15af8ac1187029799662080
Gerald Hakeem Bryant <gerald.bryant@assurant.com>
2018-09-30 07:29:55 -0400

    Updating react file locations

--- docs/scripting.jsx
+++ docs/scripting.jsx
@@ -1,5 +1,5 @@
-import React from './react';
-import ReactDOM from './react-dom';
-import App from './app.jsx';
+import React from './node_modules/react';
+import ReactDOM from './node_modules/react-dom';
+import App from './node_modules/app.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
