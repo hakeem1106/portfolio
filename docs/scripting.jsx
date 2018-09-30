5abb7f59025db12d3f20995e10b4f3f185917ca7
Gerald Hakeem Bryant <gerald.bryant@assurant.com>
2018-09-30 11:41:19 -0400

    updating, trying to get react to work

--- docs/scripting.jsx
+++ docs/scripting.jsx
@@ -1,8 +1,8 @@
-import React from './node_modules/react';
-import ReactDOM from './node_modules/react-dom';
-import App from './node_modules/app.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'app.jsx';
 
-var React = require('react');
+
 
 const greeting = React.createElement('h1',{},'This is where I will put the about me section');
-ReactDOM.render(greeting, document.getElementById('app'));
+ReactDOM.render(greeting, document.getElementById('root'));
