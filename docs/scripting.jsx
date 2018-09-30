b44549cf42a707dffbba2bef5efbf1406551627f
Gerald Hakeem Bryant <gerald.bryant@assurant.com>
2018-09-30 08:04:47 -0400

    trying

--- docs/scripting.jsx
+++ docs/scripting.jsx
@@ -2,4 +2,6 @@ import React from './node_modules/react';
 import ReactDOM from './node_modules/react-dom';
 import App from './node_modules/app.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+
+const greeting = React.createElement('h1',{},'This is where I will put the about me section');
+ReactDOM.render(greeting, document.getElementById('app'));
