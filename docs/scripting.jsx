25088caa5899e76bc660595f61d5395f37a253e1
Gerald Bryant <hakeem1106@gmail.com>
2018-10-01 15:32:17 +0000

    Removing Alll React

--- docs/scripting.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'app.jsx';
-
-
-
-const greeting = React.createElement('h1',{},'This is where I will put the about me section');
-ReactDOM.render(greeting, document.getElementById('root'));
