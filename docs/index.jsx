805b411702d9675cc552f189fb8c6a161fa8ac56
Gerald Bryant <hakeem1106@gmail.com>
2018-10-01 21:05:04 +0000

    might have fixed it

--- docs/index.jsx
+++ docs/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react;
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App(){
-    return(
+newFunction();
 
-    <div className = "App">
-    <h1> React Works</h1>
-    </div>
-    );
+function newFunction() {
+    class App {
+    }
+    () => {
+        return (<div className="App">
+            <h1> React Works</h1>
+        </div>);
+    };
+    const rootElement = document.getElementById('root');
+    ReactDOM.render(<App />, rootElement);
 }
-
-const rootElement = document.getElementById('root');
-ReactDOM.render (<App />, rootElement);
