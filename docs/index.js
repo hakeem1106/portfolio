92e547ad0cd7496d67212e7042f520fc43d70de1
Gerald Bryant <hakeem1106@gmail.com>
2018-10-02 17:10:39 +0000

    adding react one more time

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,15 @@
 // Import React and ReactDOM
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Render component into the DOM - only once per app
-//ReactDOM.render(
- //<h1>Hello, world!</h1>,
- //document.getElementById('root')
-//);
 
+class Greeting extends React.Component {
+    render() {
+        return (<p>Hello world</p>);
+    }
+};
 
+ReactDOM.render(
+    <Greeting />,
+    document.getElementById('root')
+);
