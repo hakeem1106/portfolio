5945c9bfafd376922d5fd8cfb7ce4744c0a801da
Gerald Bryant <hakeem1106@gmail.com>
2018-10-02 16:36:50 +0000

    ???

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,10 @@
 // Import React and ReactDOM
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 // Render component into the DOM - only once per app
-ReactDOM.render(
- <h1>Hello, world!</h1>,
- document.getElementById('root')
-);
+//ReactDOM.render(
+ //<h1>Hello, world!</h1>,
+ //document.getElementById('root')
+//);
 
 
